a8b18d4bae40886e851fd5adc1ecd6035d7e4067
Eko-max999 <13118@holbertonstudents.com>
2026-04-12 18:38:40 +0000

    Add 0-async_generator.py

--- 0-async_generator.py
+++ 0-async_generator.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
-"""Async generator module"""
+"""
+Asinxron generator modulu.
+"""
 import asyncio
 import random
 from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """Loops 10 times, yields a random number every second"""
+    """
+    10 dəfə dövr edir, hər dəfə 1 saniyə gözləyir
+    və 0 ilə 10 arasında təsadüfi ədəd qaytarır.
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
