f68298c8a2079af9ac66139ee3632629b6b4827f
Eko-max999 <13118@holbertonstudents.com>
2026-04-12 18:30:26 +0000

    Task 0: Async Generator

---
+++ 0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Async generator tapşırığı"""
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    10 dəfə dövr edir, hər dəfə 1 saniyə gözləyir
+    və 0-10 arası təsadüfi ədəd qaytarır.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
