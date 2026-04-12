78a9086b28c7c70727797150d153878a7388e514
Eko-max999 <13118@holbertonstudents.com>
2026-04-12 19:22:53 +0000

    Pycodestyle and structure fix

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -10,8 +10,7 @@ from typing import Generator
 
 async def async_generator() -> Generator[float, None, None]:
     """
-    10 dəfə dövr edir, hər dəfə 1 saniyə gözləyir
-    və 0 ilə 10 arasında təsadüfi ədəd qaytarır.
+    10 random numbers generator
     """
     for _ in range(10):
         await asyncio.sleep(1)
