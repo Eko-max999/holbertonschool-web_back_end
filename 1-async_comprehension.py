ddfc7ef1812a43a9b8ebbacd5374911c16c8cc91
Eko-max999 <13118@holbertonstudents.com>
2026-04-12 18:43:08 +0000

    Add 1-async_comprehension.py

---
+++ 1-async_comprehension.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Async comprehension modulu.
+"""
+from typing import List
+import importlib
+
+# 0-async_generator-u import edirik
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Asinxron generatoru işlədir və nəticələri
+    async comprehension vasitəsilə siyahı kimi qaytarır.
+    """
+    return [i async for i in async_generator()]
