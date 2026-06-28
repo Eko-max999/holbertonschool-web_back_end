5bf3348fe21d714d519aabe6d7279be44acd0c1c
Eko-max999 <13118@holbertonstudents.com>
2026-06-28 23:14:47 +0000

    Fix: Add 12-log_stats.py securely without repository conflicts

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python3
-""" Write a Python script that provides some stats about
-    Nginx logs stored in MongoDB
+"""
+Module to provide statistics about Nginx logs stored in MongoDB.
 """
 from pymongo import MongoClient
 
-if __name__ == "__main__":
-    """ Database: logs
-        Collection: nginx
+
+def log_stats():
+    """
+    Connects to MongoDB and prints detailed statistics about Nginx logs.
     """
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
 
-    n_logs = nginx_collection.count_documents({})
-    print(f'{n_logs} logs')
+    total_logs = nginx_collection.count_documents({})
+    print("{} logs".format(total_logs))
+
+    print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print('Methods:')
     for method in methods:
         count = nginx_collection.count_documents({"method": method})
-        print(f'\tmethod {method}: {count}')
+        print("\tmethod {}: {}".format(method, count))
 
     status_check = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"}
-        )
-    print(f'{status_check} status check')
+    )
+    print("{} status check".format(status_check))
+
+
+if __name__ == "__main__":
+    log_stats()
